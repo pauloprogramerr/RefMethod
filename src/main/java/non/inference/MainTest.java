2a543f4af9c8567c25e3f9888eec70accef3d240
pauloprogramerr <paulo_pauladas@hotmail.com>
2025-04-22 23:33:26 -0300

    pag 153

--- src/main/java/non/inference/MainTest.java
+++ src/main/java/non/inference/MainTest.java
@@ -5,14 +5,11 @@ public class MainTest {
     public MainTest (String m) {
        this.m = m;
     }
-    public MainTest(){
-    }
+
 
     public static void main(String[] args) {
         MainTest m = new MainTest ("Hello");
         System.out.println (m.m);
 
-        MainTest mainTest = new MainTest ();
-        System.out.println (mainTest);
     }
 }
