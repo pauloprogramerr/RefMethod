2bfddb3eadfbde47f9075d47638604b2426fb461
pauloprogramerr <paulo_pauladas@hotmail.com>
2025-04-22 23:25:00 -0300

    pag 153 p

---
+++ src/main/java/non/inference/MainTest.java
@@ -0,0 +1,16 @@
+package non.inference;
+
+public class MainTest {
+    private String m;
+    public MainTest (String m) {
+       this.m = m;
+    }
+    public MainTest(){
+    }
+
+    public static void main(String[] args) {
+        MainTest m = new MainTest ("Hello");
+        System.out.println (m.m);
+
+    }
+}
